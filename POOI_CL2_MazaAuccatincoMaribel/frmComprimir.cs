0d60ec23f6dc2aca31b6e453b36b4fe2e7705bfa
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-10-16 21:00:56 -0500

    Agregar archivos de proyecto.

---
+++ POOI_CL2_MazaAuccatincoMaribel/frmComprimir.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POOI_CL2_MazaAuccatincoMaribel
+{
+    public partial class frmComprimir : Form
+    {
+        public frmComprimir()
+        {
+            InitializeComponent();
+        }
+    }
+}
